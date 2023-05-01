004dae1f8434f5d2a35974e185301549ea41af76
JuliaHa0902 <JuliaHa0902@gmail.com>
2023-05-01 11:37:59 -0500

    Add API endpoint

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_paXSDzHn6', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4eg3m4ebbnhovasgqkrjjvfonb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7mq6ka2ad74cus3202jke596hu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://mt0lhprb30.execute-api.us-east-2.amazonaws.com/prod',
     }
 };
